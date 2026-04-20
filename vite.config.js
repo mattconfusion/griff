13f2302aac2555efd063f05a1ae9f8ff13a281ce
Matteo Radice <mattconfusion@hotmail.com>
2026-04-20 23:44:29 +0200

    Fixed deploy

--- vite.config.js
+++ vite.config.js
@@ -4,4 +4,5 @@ import react from '@vitejs/plugin-react'
 // https://vite.dev/config/
 export default defineConfig({
   plugins: [react()],
+  base: './', // Using relative path is safer for most GH Pages setups
 })
